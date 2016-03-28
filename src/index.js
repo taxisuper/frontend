3fc9c21e345db462072f4c3c499aed779eaabb73
Ingar Almklov <ingara@gmail.com>
2016-03-28 14:02:39 +0200

    Reduxify

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
-import App from './components/App';
+import configureStore from './configureStore';
+import { newTweet } from './actions';
 
-render(<App />, document.querySelector('#app'));
+import App from './containers/App';
+import DevTools from './containers/DevTools';
+
+const store = configureStore();
+
+const ws = new WebSocket('ws://twitterws.herokuapp.com');
+
+ws.onmessage = ms => {
+  const tweet = JSON.parse(ms.data);
+
+  store.dispatch(newTweet(tweet));
+};
+
+render(
+  <Provider store={ store }>
+    <div>
+      <App />
+      <DevTools />
+    </div>
+  </Provider>,
+  document.querySelector('#app')
+);
