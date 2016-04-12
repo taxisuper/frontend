882a244ba1741f1398b04d5d7fc1f754ee0a6442
Guro <guro.seternes@bekk.no>
2016-04-12 19:38:50 +0200

    Removed tweetCount from view state Removed geoMatcher

--- src/reducers/view.js
+++ src/reducers/view.js
@@ -5,29 +5,15 @@ import {
 } from '../actions';
 
 const initialState = {
-  tweetCount: 0,
   currentTweet: null,
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
-  case TWEET_RECEIVED:
-    return {
-      ...state,
-      tweetCount: state.tweetCount + 1
-    };
   case TWEET_SELECTED:
     return {
       ...state,
       currentTweet: action.tweet
-    };
-  case FILTER_DELETED:
-    return {
-      ...state,
-      activeFilterMap: {
-        ...state.activeFilterMap,
-        [action.filter.name]: false
-      }
     };
   default:
     return state;
