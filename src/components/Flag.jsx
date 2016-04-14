6d0e7ef3e596206036c5a334586f07064a76d18e
Erik Wendel <erik@wendel.no>
2016-04-14 17:10:20 +0200

    removed solution and wiped starter files

--- src/components/Flag.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Flag({ countryCode }) {
-  return (
-    <span className={`tweet-flag flag-icon flag-icon-${countryCode.toLowerCase()}`}></span>
-  );
-}
