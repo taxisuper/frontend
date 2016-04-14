078582540efa0eac5b1da79ce0ebf2c9cff9f8a8
Guro <guro.seternes@bekk.no>
2016-04-14 19:35:55 +0200

    FilterForm is now a smart connected component The form data is now stored in the store, instead of being a part of the local state Created Task 8

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,10 +4,12 @@ import tweets from './tweets';
 import countries from './countries';
 import view from './view';
 import filters from './filters';
+import form from './form';
 
 export default combineReducers({
   tweets,
   countries,
   view,
+  form,
   filters
 });
